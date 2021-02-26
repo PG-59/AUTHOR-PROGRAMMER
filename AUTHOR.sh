8ce3376a9ee857af5015ca2e386f054f77309976
PG-59 <79249195+PG-59@users.noreply.github.com>
2021-02-26 23:55:20 +0800

    Create AUTHOR.sh

---
+++ AUTHOR.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#author:Prince Gutierrez
+
+#################################
+#     Project : Hackerprince    #
+#     author : PRINCE GUTIERREZ #
+#################################
+
+
+  ## Banner
+  banner() {
+          cat <<- EOF
+                ${ORANGE}########  ########
+                ${ORANGE}#      #  #
+                ${ORANGE}#      #  #
+                ${ORANGE}########  # ######
+                ${ORANGE}#         #      #
+                ${ORANGE}#         #      #
+                ${ORANGE}#         ########
+    
+ ${GREEN}[${WHITE}-${GREEN}]${CYAN} HI I AM {PRINCE GUTIERREZ}
+ ${GREEN}[${WHITE}-${GREEN}]${CYAN} FROM PHILLIPINES 11 YEARS OLD HACKER AND PROGRAMMER PLEASE SUPPORT MEFROM PHILLIPINES 11 YEARS OLD HACKER AND PROGRAMMER PLEASE SUPPORT ME
+ ${GREEN}[${WHITE}-${GREEN}]${CYAN} GITHUB ACCOUNT: https://github.com/PG-59

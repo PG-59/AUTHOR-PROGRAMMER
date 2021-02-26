cd0d034f5c893bef033e9c7ff1049e9d59839efc
PG-59 <79249195+PG-59@users.noreply.github.com>
2021-02-26 23:45:19 +0800

    Create author.sh
    h

---
+++ author.sh
@@ -0,0 +1,22 @@
+#!/data/data/com.termux/files/usr/bin/bash
+#Downloading the req dependencies to run a script without error
+#################################
+#     Project : Hackerprince    #
+#     author : PRINCE GUTIERREZ #
+#################################
+
+
+  ## Banner
+  banner() {
+          cat <<- EOF
+                ${ORANGE}########  ########
+                ${ORANGE}#      #  #
+                ${ORANGE}#      #  #
+                ${ORANGE}########  # ######
+                ${ORANGE}#         #      #
+                ${ORANGE}#         #      #
+                ${ORANGE}#         ########
+    
+ ${GREEN}[${WHITE}-${GREEN}]${CYAN} HI I AM {PRINCE GUTIERREZ}
+ ${GREEN}[${WHITE}-${GREEN}]${CYAN} FROM PHILLIPINES 11 YEARS OLD HACKER AND PROGRAMMER PLEASE SUPPORT MEFROM PHILLIPINES 11 YEARS OLD HACKER AND PROGRAMMER PLEASE SUPPORT ME
+ ${GREEN}[${WHITE}-${GREEN}]${CYAN} GITHUB ACCOUNT: https://github.com/PG-59

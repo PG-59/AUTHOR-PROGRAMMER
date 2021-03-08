4b3955cdb17f92c659a9751edf5fa76d758627d0
PG-59 <79249195+PG-59@users.noreply.github.com>
2021-03-09 02:02:44 +0800

    Create author.sh

---
+++ author.sh
@@ -0,0 +1,24 @@
+##!/bin/bash
+##author 
+##coded by https://github.com/PG-59
+##I WILL REPRESENT MY SELF USING THE AUTHOR-PROGRAMMER
+
+echo    I AM PRINCE GUTIERREZ 
+echo    I AM IN PHILLIPINES
+echo    I AM HACKER AND PROGRAMMER IN PHILLIPINES
+echo    MY SKILLS BASH & PYTHON
+echo    MY GITHUB:https://github.com/PG-59
+
+echo   ########  ########
+echo   #      #  #
+echo   #      #  #
+echo   ########  # ######
+echo   #         #      #
+echo   #         #      #
+echo   #         ########
+echo 
+am start -a android.intent.action.VIEW -d https://github.com/PG-59>> /dev/null 2>&1
+echo PLEASE SUBSCRIBE MY CHANNEL
+echo PHILLIPINES IS THE BEST HACKER
+
+

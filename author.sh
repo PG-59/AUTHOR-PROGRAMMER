0fbdfccdcae4b0b9069529ec9a4b574f17d7dfa5
PG-59 <79249195+PG-59@users.noreply.github.com>
2021-03-09 02:16:14 +0800

    Update author.sh

--- author.sh
+++ author.sh
@@ -7,18 +7,11 @@ echo    I AM PRINCE GUTIERREZ
 echo    I AM IN PHILLIPINES
 echo    I AM HACKER AND PROGRAMMER IN PHILLIPINES
 echo    MY SKILLS BASH & PYTHON
-echo    MY GITHUB:https://github.com/PG-59
-
-echo   ========= =========
-echo   ||     || ||
-echo   ||     || ||
-echo   ||=====|| ||======||
-echo   ||        ||      ||  
-echo   ||        ||      ||
-echo   ||        ||========
-echo 
+echo    MY GITHUB:https://github.com/PG-59 
 am start -a android.intent.action.VIEW -d https://github.com/PG-59>> /dev/null 2>&1
 echo PLEASE SUBSCRIBE MY CHANNEL
 echo PHILLIPINES IS THE BEST HACKER
+am start -a android.intent.action.VIEW -d https://m.youtube.com/channel/UCojp00wv_BKkkFRHPNnIrJQ>> /dev/null 2>&1
+
 
 

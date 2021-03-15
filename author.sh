2a19c4a19adb06291ed2397561d0725372865537
PG-59 <79249195+PG-59@users.noreply.github.com>
2021-03-15 09:53:05 +0800

    Update author.sh

--- author.sh
+++ author.sh
@@ -1,17 +1,19 @@
 ##!/bin/bash
-##author 
+##author PRINCE GUTIERREZ
 ##coded by https://github.com/PG-59
 ##I WILL REPRESENT MY SELF USING THE AUTHOR-PROGRAMMER
 
-echo    I AM PRINCE GUTIERREZ 
-echo    I AM IN PHILLIPINES
-echo    I AM HACKER AND PROGRAMMER IN PHILLIPINES
-echo    MY SKILLS BASH & PYTHON
-echo    MY GITHUB:https://github.com/PG-59 
+printf "\e[1;91m    I AM PRINCE GUTIERREZ\e[0m\n"
+printf "\e[1;92m    I AM IN PHILLIPINES\e[0m\n"
+printf "\e[1;93m    I AM HACKER AND PROGRAMMER IN PHILLIPINES\e[0m\n"
+printf "\e[1;94m    MY SKILLS BASH & PYTHON\e[0m\n"
+printf "\e[1;95m    MY GITHUB:https://github.com/PG-59\e[0m\n" 
 am start -a android.intent.action.VIEW -d https://github.com/PG-59>> /dev/null 2>&1
-echo PLEASE SUBSCRIBE MY CHANNEL
-echo PHILLIPINES IS THE BEST HACKER
+printf "\e[1;96m PLEASE SUBSCRIBE MY CHANNEL\e[0m\n"
+printf "\e[1;97m PHILLIPINES IS THE BEST HACKER\e[0m\n"
 am start -a android.intent.action.VIEW -d https://m.youtube.com/channel/UCojp00wv_BKkkFRHPNnIrJQ>> /dev/null 2>&1
+
+
 
 
 
